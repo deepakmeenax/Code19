3d3104345080e9360841d0044c10dc04bf693154
Deepak Meena <memarsahab@gmail.com>
2020-05-01 20:00:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const request=require('request')
+const express = require('express')
+const path = require('path')
+const publicDirectory = path.join(__dirname,'/public')
+
+const app = express();
+app.use(express.static(publicDirectory))
+
+const port = process.env.PORT || 3000
+
+var options = {
+    method: 'GET',
+    url: 'https://covid-193.p.rapidapi.com/statistics',
+    headers: {
+      'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+      'x-rapidapi-key':process.env.API_KEY
+    },
+    proxy:process.env.PROXY || null
+  };
+
+ 
+const covidCall = (callback) =>{
+request(options, function (error, response, body) {
+    if (error) {
+      return callback('error',undefined)
+    }
+    
+    data=JSON.parse(body);
+    callback(undefined,data);
+    
+
+});
+}
+app.get('/covidata',(req,res)=>{
+    covidCall(callback = (error,data)=>{
+      if(error)
+      {
+        return res.send(error)
+      }
+      res.send(data)
+
+    })
+})
+
+app.listen(port,()=>{
+    console.log('server is started on port',port);
+})
---
+++ public/script.js
@@ -0,0 +1,63 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+}
+
+
+
+
+function fun(){
+    fetch('/covidata').then((response)=>{
+        response.json().then((data)=>{
+            
+          
+        var myTable=document.getElementById("myTable");
+
+        for(var i=1;i<=226;i++)
+        {
+          var x=myTable.insertRow(i);
+          
+          x.insertCell(0);
+          myTable.rows[i].cells[0].innerHTML=data.response[i-1].country;
+          
+          x.insertCell(1);
+          myTable.rows[i].cells[1].innerHTML=data.response[i-1].cases.total;
+    
+          x.insertCell(2);
+          myTable.rows[i].cells[2].innerHTML=data.response[i-1].cases.recovered;
+          
+          x.insertCell(3);
+          myTable.rows[i].cells[3].innerHTML=data.response[i-1].cases.active;
+        
+          x.insertCell(4);
+          myTable.rows[i].cells[4].innerHTML=data.response[i-1].deaths.total;
+          
+          x.insertCell(5);
+          myTable.rows[i].cells[5].innerHTML= 0 | data.response[i-1].tests.total;
+    
+        }
+
+
+
+        })
+    })
+    
+
+}
